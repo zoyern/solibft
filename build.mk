f6080e8d2e15a3aeaeb242c393469a4799a4c104
Zoyerny <zoyern@gmail.com>
2024-04-26 02:22:17 +0200

    try to compile solibft and sotypes

--- build.mk
+++ build.mk
@@ -3,18 +3,16 @@
 #                                                         :::      ::::::::    #
 #    build.mk                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: marvin <marvin@student.42.fr>              +#+  +:+       +#+         #
+#    By: almounib <almounib@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/02 23:07:18 by marvin            #+#    #+#              #
-#    Updated: 2024/04/02 23:07:18 by marvin           ###   ########.fr        #
+#    Updated: 2024/04/26 02:20:10 by almounib         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 include src/src.mk
 include exemple/exemple.mk
 
-#-I./$(BUILD_INCLUDES)
-## include "solibft/all.h"
 #❖══════Setup══════❖
 NAME		= solibft
 
@@ -26,10 +24,18 @@ LIB			= ranlib
 
 #❖══════Build══════❖
 BUILD_DIR		= build
-SOLIBFT			= $(BUILD_DIR)/$(NAME).a
+LIBRARY			= $(BUILD_DIR)/$(NAME).a
 BUILD_OBJ		= $(BUILD_DIR)/obj
 BUILD_INCLUDES	= $(BUILD_DIR)/includes
-ALL_INCLUDES	= $(BUILD_INCLUDES)/solibft
+ALL_B_INCLUDES	= $(BUILD_INCLUDES)/$(NAME)
+
+#❖══════Libs══════❖
+LIBS_DIR		= libs
+LIBS_NAMES		= sotypes hello
+LIBRARYS		= $(addsuffix .a, $(addprefix $(LIBS_DIR)/$(BUILD_DIR)/, $(LIBS_NAMES)))
+LIBS_OBJ		= $(LIBS_DIR)/$(LIBS_NAMES)/$(BUILD_DIR)//obj
+LIBS_INCLUDES	= $(LIBS_DIR)/$(LIBS_NAMES)/$(BUILD_DIR)/includes
+ALL_L_INCLUDES	= $(LIBS_INCLUDES)/$(LIBS_NAMES)
 
 #❖══════Objet══════❖
 OBJ = $(patsubst $(SRC_DIR)/%.c,$(BUILD_OBJ)/%.o, $(SRC))
@@ -45,13 +51,14 @@ $(BUILD_OBJ)/%.o: $(SRC_DIR)/%.c
 	$(CC) $(CFLAG) -c $< -o $@
 
 $(BUILD_INCLUDES):
+	@echo "\n\n\n\n\n" $(LIBRARYS) "\n\n\n\n\n"
 	@mkdir -p $(BUILD_INCLUDES)
-	@mkdir -p $(ALL_INCLUDES)
+	@mkdir -p $(ALL_B_INCLUDES)
 	@cp $(LIB_HEADER) $(BUILD_INCLUDES)
-	@cp $(HEADERS) $(ALL_INCLUDES)
+	@cp $(HEADERS) $(ALL_B_INCLUDES)
 
 #❖═════Creat═════❖
 $(NAME): $(OBJ)
-	${AR} $(SOLIBFT) ${OBJ}
-	${LIB} $(SOLIBFT)
-	@$(CC) $(SRC_EXEMPLE) $(OBJ) -o $(NAME) $(CFLAG) $(SOLIBFT)
+	@${AR} $(LIBRARY) ${OBJ}
+	@${LIB} $(LIBRARY)
+	@$(CC) $(SRC_EXEMPLE) $(OBJ) -o $(NAME) $(CFLAG) $(LIBRARY)
