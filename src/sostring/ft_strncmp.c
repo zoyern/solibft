8e46d5b45e67475fb73886dba123a46bb78f2be2
zoyern <zoyern@gmail.com>
2024-08-31 22:31:43 +0200

    correct strncmp

--- src/sostring/ft_strncmp.c
+++ src/sostring/ft_strncmp.c
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n < 1)
 		return (0);
-	while (s1[i] && s2[i] && (s1[i] == s2[i]) && i < (n))
+	while (s1[i] && s2[i] && (s1[i] == s2[i]) && i < n)
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
